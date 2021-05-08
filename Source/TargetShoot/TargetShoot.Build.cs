38b566c2dba7731f6cb42d3b3f257c02dc44189f
samalo0 <stephen.a.maloney@gmail.com>
2021-05-08 15:54:23 -0400

    Removed roughness and normal and set the master material to full rough. This cuts the instructions in half for all the materials.

--- Source/TargetShoot/TargetShoot.Build.cs
+++ Source/TargetShoot/TargetShoot.Build.cs
@@ -18,13 +18,5 @@ public class TargetShoot : ModuleRules
 			var manifestFile = Path.Combine(ModuleDirectory, "AndroidSanitizePermissions_UPL.xml");
 			AdditionalPropertiesForReceipt.Add("AndroidPlugin", manifestFile);
 		}
-		
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
